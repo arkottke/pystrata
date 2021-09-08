af3867b7706ce896521df36c22a833a009f6ad8d
Albert Kottke <albert.kottke@gmail.com>
2021-09-07 20:29:56 -0700

    Added pykooh requirement.

--- setup.py
+++ setup.py
@@ -3,43 +3,45 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as fp:
+with open("README.rst") as fp:
     readme = fp.read()
 
-with open('HISTORY.rst') as fp:
+with open("HISTORY.rst") as fp:
     history = fp.read()
 
 setup(
-    name='pySRA',
-    version='0.4.11',
-    description='Site Response Analysis with Python',
-    long_description=readme + '\n\n' + history,
-    author='Albert Kottke',
-    author_email='albert.kottke@gmail.com',
-    url='http://github.com/arkottke/pysra',
+    name="pySRA",
+    version="0.4.11",
+    description="Site Response Analysis with Python",
+    long_description=readme + "\n\n" + history,
+    author="Albert Kottke",
+    author_email="albert.kottke@gmail.com",
+    url="http://github.com/arkottke/pysra",
     packages=find_packages(),
     install_requires=[
-        'matplotlib',
-        'numpy',
-        'pyrvt',
-        'scipy',
-        'setuptools',
-        'typing',
+        "matplotlib",
+        "numpy",
+        "pyrvt",
+        "pykooh",
+        "scipy",
+        "setuptools",
+        "typing",
     ],
     extras_require={
-        'dataframe': ['pandas'],
+        "dataframe": ["pandas"],
     },
-    keywords='site response',
-    license='MIT',
+    keywords="site response",
+    license="MIT",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering',
-        'Intended Audience :: Science/Research',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
     ],
-    test_suite='tests', )
+    test_suite="tests",
+)
